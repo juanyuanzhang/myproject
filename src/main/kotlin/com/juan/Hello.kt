f60f48d5e1b59f7ae5b958d2eb2258acab96ce51
juanyuanzhang <40617047+juanyuanzhang@users.noreply.github.com>
2020-07-01 00:48:19 +0800

    Initial commit

---
+++ src/main/kotlin/com/juan/Hello.kt
@@ -0,0 +1,14 @@
+package com.juan
+
+fun main(args : Array<String>) {
+    //println("HELLO Kotlin")
+    //Human().hello()
+    val h = Human()
+    h.hello()
+}
+
+class Human {
+    fun hello(){
+        println("HELLO Kotlin")
+    }
+}
